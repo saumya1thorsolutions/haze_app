0402c317fd3c748f179c54e58d1fc77a634c0d11
saumya1thorsolutions <saumya@thor.solutions>
2023-11-16 10:46:40 +0530

    error handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,11 +6,21 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error,setError] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    signInWithEmailAndPassword(auth, email, password);
-    navigate("/home");
+  const handleLogin = async () => {
+    try{
+      await auth.signInWithEmailAndPassword(email,password);
+      navigate("/home");
+    }catch(error){
+      console.log("error code",error);
+      if (error.code === 'auth/wrong-password') {
+        setError('Incorrect password. Please try again.');
+      } else {
+        setError('An error occurred. Please try again later.');
+      }
+    }    
   };
   return (
     <div className="login">
@@ -29,6 +39,7 @@ const Login = () => {
         placeholder="Password"
       />
       <button onClick={handleLogin}>Log in</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
 };
